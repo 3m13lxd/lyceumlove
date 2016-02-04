d438395ae1b5ccd553771dfe7d0123995a38440d
3m13lxd <3m13lxd@gmail.com>
2016-02-04 22:57:40 +0100

    Create submit.php

---
+++ submit.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Hai <?php echo $_POST["naam"]; ?><br>
+Je achternaam is: <?php echo $_POST["achternaam"]; ?><br>
+Je geslacht is: <?php echo $_POST["gender"]; ?>
+
+</body>
+</html>
